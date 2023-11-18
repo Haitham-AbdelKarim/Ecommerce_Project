79e0af7e50a227aef0506572b5d956a9de0fb64a
Haitham-AbdelKarim <haithammoabdelkarim@gmail.com>
2023-11-18 19:02:39 +0200

    Add EcommerceContext file

---
+++ Ecommerce_Project/Models/EcommerceContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace Ecommerce_Project.Models
+{
+    public class EcommerceContext : DbContext
+    {
+        private string _connString;
+        public EcommerceContext()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false);
+            IConfiguration configuration = builder.Build();
+            _connString = configuration.GetValue<string>("ConnectionStrings:value");
+        }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connString);
+        }
+
+
+    }
+}
